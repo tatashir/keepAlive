856e804bca460af11300cab20c46402d645806f6
tatashir <tshirotai9683@gmail.com>
2024-02-08 19:19:04 +0000

    fix

--- keepAlive.sh
+++ keepAlive.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+start_time=$(date +%s)
 while true; do
-    echo "Keep alive" 
-    sleep 900
+    current_time=$(date +%s)
+    elapsed_time=$((current_time - start_time))
+    clear
+    echo -e "Elapsed time: $((elapsed_time / 60)) minutes $((elapsed_time % 60)) seconds"
+    sleep 1
 done
-
